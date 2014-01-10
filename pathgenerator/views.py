746cd18d346bdfd7ded483d3e0494559318cda44
Dustin Doss <dustinddoss@gmail.com>
2014-01-10 13:01:32 -0500

    In new ddoss-exp branch, made new map html template to connect to Google Maps API, linked it through views.py and urls.py, and modified settings.py to use the templates directory.

--- pathgenerator/views.py
+++ pathgenerator/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.template import Template, Context
+from django.http import HttpResponse
+from django.template.loader import get_template
 
 # Create your views here.
+def map_page(request):
+	t = get_template('map.html')
+	html = t.render(Context({}))
+	return HttpResponse(html)
--- streetfarer/urls.py
+++ streetfarer/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from pathgenerator.views import map_page
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^map/$', map_page),
     # Examples:
     # url(r'^$', 'streetfarer.views.home', name='home'),
     # url(r'^streetfarer/', include('streetfarer.foo.urls')),
