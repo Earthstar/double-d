cb6f987b655256049a4bce2b0f627e8e21a63470
Deena Wang <thedeenawang@gmail.com>
2014-01-12 14:18:32 -0500

    Adds google_reference field to Place

--- pathgenerator/models.py
+++ pathgenerator/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+# drops all tables. Useful if you've added a field.
+# There is a "proper" way to add a field, but it's more complicated
+# See: http://www.djangobook.com/en/2.0/chapter10.html
+# section Making Changes to a Database Schema
+# python manage.py sqlclear pathgenerator | python manage.py dbshell
+
 class Place(models.Model):
     '''
     Represents a place that could be added to a path
@@ -12,6 +18,8 @@ class Place(models.Model):
     # Only looking at string-formatted address for now
     address = models.TextField()
     name = models.TextField()
+    # Use this field to store Google's reference value
+    google_reference = models.TextField()
     # Use this to store total data, ex the Json from Google API
     # Just in case we need to extract another field for the DB
     raw_data = models.TextField()
@@ -29,6 +37,7 @@ class Tag(models.Model):
     Represents a feature of a place.
     Each tag may belong to multiple places and vice versa
     all characters are lowercase
+    Could bootstrap with Google place types
     '''
     tag = models.TextField()
     places = models.ManyToManyField(Place)
