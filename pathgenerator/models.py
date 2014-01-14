aaa356025c8e296fc3ff1bec72951a5f7aee6933
Deena Wang <thedeenawang@gmail.com>
2014-01-14 11:15:04 -0500

    Adds Path model, adds test

--- pathgenerator/models.py
+++ pathgenerator/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from ordered_model.models import OrderedModel
 
-# drops all tables. Useful if you've added a field.
+
 # There is a "proper" way to add a field, but it's more complicated
 # See: http://www.djangobook.com/en/2.0/chapter10.html
 # section Making Changes to a Database Schema
+# drops all tables. Useful if you've added a field to a model
 # python manage.py sqlclear pathgenerator | python manage.py dbshell
+
+# Users can store many Paths (which can be shared?)
+# Each path contains a list of Places and a name
+# Places can be in multiple Paths and must be ordered in a Path
 
 class Place(models.Model):
     '''
@@ -24,23 +30,43 @@ class Place(models.Model):
     # Just in case we need to extract another field for the DB
     raw_data = models.TextField()
 
-
-
     def __unicode__(self):
         '''
         What gets displayed when you print the place.
         '''
-        return self.name, self.address
+        return '{0}, {1}'.format(str(self.name) + str(self.address))
 
 class Tag(models.Model):
     '''
     Represents a feature of a place.
     Each tag may belong to multiple places and vice versa
     all characters are lowercase
-    Could bootstrap with Google place types
     '''
     tag = models.TextField()
     places = models.ManyToManyField(Place)
 
     def __unicode__(self):
         return self.tag
+
+# Create model that stores a path
+class Path(models.Model):
+    '''
+    Represents an ordered list of places that make a path.
+    Django models automatically get an autoincrementing id field
+    which is created when you save the model.
+    Use that?
+    To find a particular path, use the id field
+    path = Path.objects.get(id=1)
+    To get all the places on a path,
+    places = path.places.all()
+    To get the ordering of places, filter PlaceOrdering
+    PlaceOrdering.objects.filter(path_id__exact=1, place_id__exact=1)
+    Is there a better way of doing this?
+    '''
+    name = models.TextField()
+    places = models.ManyToManyField(Place)
+    # The json which is returned when Google routes a path
+    json = models.TextField()
+
+    def __unicode__(self):
+        return self.name
--- pathgenerator/tests.py
+++ pathgenerator/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+import pathgenerator.models as pg
 
-# Create your tests here.
+class DatabaseTestCase(TestCase):
+    '''
+    Uses initial_data fixture
+    '''
+
+    def setUp(self):
+        pass
+
+    def test_calling_path(self):
+        '''
+        trying to generate a path with ordering
+        '''
+        # get correct path
+        path_name = 'east campus loop'
+        path = pg.Path.objects.get(name=path_name)
+        self.assertIsNotNone(path)
+        # Get all the places in the path
+        places = path.places.all() #this will be a QuerySet
+        self.assertEqual(3, places.count()) # use count instead of len
