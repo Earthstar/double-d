d723d5f6b7852050ee373f7db7f4df314ab3a06b
Deena Wang <thedeenawang@gmail.com>
2014-01-15 12:57:10 -0500

    Adds login logout loop with secret pages

--- streetfarer/urls.py
+++ streetfarer/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login
 
 from pathgenerator.views import map_page, mapsearch_page, logout_page, success_page
-from streetfarer.views import add_user
+from streetfarer.views import add_user, secret
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     url(r'^logout/$', logout_page),
     url(r'^success/$', success_page),
     url(r'^adduser/$', add_user),
+    url(r'^secret/$', secret),
     # Examples:
     url(r'^$', 'streetfarer.views.home', name='home'),
 
--- streetfarer/views.py
+++ streetfarer/views.py
@@ -1,16 +1,17 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
-
 from django.core.context_processors import csrf
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, render_to_response
+from django.template import Template, Context, RequestContext
+from django.template.loader import get_template
 
 from pathgenerator.models import UserForm
 
 def home(request):
-    return HttpResponse('Home page')
+    t = get_template('index.html')
+    html = t.render(Context({}))
+    return HttpResponse(html)
 
 def add_user(request):
     '''
@@ -36,16 +37,18 @@ def add_user(request):
             user = authenticate(username=request.POST['username'], password=request.POST['password'])
             # Since user was just created, don't bother doing existence checks
             login(request, user)
-
-            # if user is not None:
-            #     if user.is_active:
-            #         login(request, user)
-            #         return HttpResponseRedirect('/success/')
-            #     else:
-            #         return HttpResponse('Disabled Account')
-            # else:
-            #     return HttpResponse('Invalid login')
     else:
         form = UserForm()
 
     return render(request, 'adduser.html', {'form': form})
+
+# todo use decorator
+def secret(request):
+    if request.user.is_authenticated():
+        user = request.user
+        t = get_template('secret.html')
+        html = t.render(Context({'user': user}))
+    else:
+        t = get_template('index.html')
+        html = t.render(Context({}))
+    return HttpResponse(html)
