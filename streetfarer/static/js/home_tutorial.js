f63232223d2db63f2bfc056d47eea11f518a844b
Deena Wang <thedeenawang@gmail.com>
2014-01-26 15:18:59 -0500

    Changes aspect ratio, fixes performance of slidedown

--- streetfarer/static/js/home_tutorial.js
+++ streetfarer/static/js/home_tutorial.js
@@ -1,8 +1,8 @@
 $(function() {
   var start;
   var mapOnScreen = false;
+  var elementsToToggleOnMapAppearance = "#map-creation-bar, .place-tag-container";
 
-  var elementsToToggleOnMapAppearance = "#map-creation-bar *, .place-tag-container, .place-tag-container > *";
 
   cambridge  = new google.maps.LatLng(42.356448, -71.108212);
 
@@ -14,29 +14,31 @@ $(function() {
   directionsDisplay.setMap(map);
 
   // Hack to get google map to appear.
-  $(".carousel-control").click(function() {
-    setTimeout(function() {
-    }, 100)
-  })
+  // $(".carousel-control").click(function() {
+  //   setTimeout(function() {
+  //   }, 100)
+  // })
 
   // Constantly updates whether map on screen?
   // Triggers events?
   // TODO make event triggers better
   setInterval(function() {
-    console.log("in polling function");
     var mapOnScreenNow = isMapOnScreen()
     if ((mapOnScreen) && (!mapOnScreenNow)) {
       // Map no longer on screen, trigger mapoff event
       console.log("trigger mapdisappear")
       $(elementsToToggleOnMapAppearance).trigger("mapdisappear")
+
     } else if ((!mapOnScreen) && (mapOnScreenNow)) {
-      console.log("trigger mapappear")
+      google.maps.event.trigger(map, 'resize')
+      setTimeout(function() {
+        console.log("trigger mapappear")
       $(elementsToToggleOnMapAppearance).trigger("mapappear")
       // refresh map
-      google.maps.event.trigger(map, 'resize')
+    }, 1000)
     }
     mapOnScreen = mapOnScreenNow;
-  },500)
+  }, 500)
 
   function isMapOnScreen() {
     return ($("#map-container").offset().top > 0)
@@ -44,6 +46,7 @@ $(function() {
 
   // If the map is on the screen, trigger the resize event and pull down the menu
   // If the map is not on the screen, pull up the menu
+  // Problem: this code causes the map to stutter.
   $(elementsToToggleOnMapAppearance).on("mapappear", function() {
     $(elementsToToggleOnMapAppearance).slideDown("fast");
     // Hide the checkbox
@@ -53,6 +56,5 @@ $(function() {
   })
 
   $(elementsToToggleOnMapAppearance).hide("fast")
-
 
 })
